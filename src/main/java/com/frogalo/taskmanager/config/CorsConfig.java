06326be8f66485721027bf724f233e4a2b7c8b85
Jakub Urbanski <s23525@pjwstk.edu.pl>
2023-05-23 00:01:18 +0200

    added api config support

---
+++ src/main/java/com/frogalo/taskmanager/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.frogalo.taskmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Replace with your specific allowed origin patterns
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
+
